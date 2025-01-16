13edc98eaff0695e56e77f7eff3655ae8e8a35a1
Gaurav Kumar <gauravkumar@Gauravs-MacBook-Pro.local>
2025-01-16 08:34:31 +0530

    Data synchronisation

--- react/query/src/lessons/fundamentals/2-query-setup.tsx
+++ react/query/src/lessons/fundamentals/2-query-setup.tsx
@@ -1,5 +1,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { QueryDependancy } from "./6-query-dependancy";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
@@ -7,6 +8,7 @@ export function QueryContext() {
   return (
     <QueryClientProvider client={queryClient}>
       <QueryDependancy />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }

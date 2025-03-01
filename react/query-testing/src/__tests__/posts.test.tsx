37103e92cceeb4a7072195e7130b9296b4a92c0b
gk9848970 <gk9848970@gmail.com>
2025-03-01 10:05:43 +0530

    Testing query with mock server

--- react/query-testing/src/__tests__/posts.test.tsx
+++ react/query-testing/src/__tests__/posts.test.tsx
@@ -1,8 +1,10 @@
-import { describe, expect, test } from "vitest";
+import { afterAll, afterEach, beforeAll, describe, expect, test } from "vitest";
 import { render, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { PostComponent } from "../posts";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { setupServer } from "msw/node";
+import { http, HttpResponse } from "msw";
 
 const renderWithQueryClient = (ui: React.ReactElement) => {
   const testQueryClient = new QueryClient({
@@ -18,8 +20,8 @@ const renderWithQueryClient = (ui: React.ReactElement) => {
   );
 };
 
-describe("App", () => {
-  test("renders learn react link", async () => {
+describe("Testing query without mock server", () => {
+  test("Renders post", async () => {
     const screen = renderWithQueryClient(<PostComponent postId={1} />);
     expect(screen.getByText(/Loading/i)).toBeInTheDocument();
     await waitFor(() => {
@@ -29,5 +31,33 @@ describe("App", () => {
         )
       ).toBeInTheDocument();
     });
+  });
+});
+
+const server = setupServer(
+  http.get("https://jsonplaceholder.typicode.com/posts/1", () => {
+    return HttpResponse.json({
+      postId: 1,
+      id: 1,
+      title:
+        "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+      body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+    });
+  })
+);
+
+describe("Testing query with mock server", () => {
+  beforeAll(() => server.listen());
+  afterEach(() => server.resetHandlers());
+  afterAll(() => server.close());
+
+  test("Renders post", async () => {
+    const screen = renderWithQueryClient(<PostComponent postId={1} />);
+    expect(await screen.findByText(/Loading/i)).toBeInTheDocument();
+    expect(
+      await screen.findByText(
+        /sunt aut facere repellat provident occaecati excepturi optio reprehenderit/i
+      )
+    ).toBeInTheDocument();
   });
 });

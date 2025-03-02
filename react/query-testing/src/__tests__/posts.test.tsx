38366a09abf010ae16a025224146345214f7d4b8
gk9848970 <gk9848970@gmail.com>
2025-03-02 08:40:58 +0530

    Test for error and events

--- react/query-testing/src/__tests__/posts.test.tsx
+++ react/query-testing/src/__tests__/posts.test.tsx
@@ -1,5 +1,5 @@
 import { afterAll, afterEach, beforeAll, describe, expect, test } from "vitest";
-import { render, waitFor } from "@testing-library/react";
+import { fireEvent, render, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { PostComponent } from "../posts";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -43,6 +43,14 @@ const server = setupServer(
         "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
       body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
     });
+  }),
+  http.get("https://jsonplaceholder.typicode.com/posts/2", () => {
+    return HttpResponse.json({
+      postId: 2,
+      id: 2,
+      title: "qui est esse",
+      body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla",
+    });
   })
 );
 
@@ -53,11 +61,34 @@ describe("Testing query with mock server", () => {
 
   test("Renders post", async () => {
     const screen = renderWithQueryClient(<PostComponent postId={1} />);
+
+    // First post
     expect(await screen.findByText(/Loading/i)).toBeInTheDocument();
     expect(
       await screen.findByText(
         /sunt aut facere repellat provident occaecati excepturi optio reprehenderit/i
       )
     ).toBeInTheDocument();
+
+    // Second post
+    fireEvent.click(screen.getByText(/Next Post/i));
+
+    expect(await screen.findByText(/Loading/i)).toBeInTheDocument();
+    expect(
+      await screen.findByText(/est rerum tempore vitae/i)
+    ).toBeInTheDocument();
+  });
+
+  test("Renders post with error", async () => {
+    server.use(
+      http.get("https://jsonplaceholder.typicode.com/posts/1", () => {
+        return HttpResponse.error();
+      })
+    );
+
+    const screen = renderWithQueryClient(<PostComponent postId={1} />);
+
+    expect(await screen.findByText(/Loading/i)).toBeInTheDocument();
+    expect(await screen.findByText(/Error/i)).toBeInTheDocument();
   });
 });
--- react/query-testing/src/posts.tsx
+++ react/query-testing/src/posts.tsx
@@ -1,11 +1,14 @@
 import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
 
 export const PostComponent = ({ postId }: { postId: number }) => {
+  const [id, setId] = useState(postId);
+
   const post = useQuery({
-    queryKey: ["posts", postId],
+    queryKey: ["posts", id],
     queryFn: async () => {
       const response = await fetch(
-        `https://jsonplaceholder.typicode.com/posts/${postId}`
+        `https://jsonplaceholder.typicode.com/posts/${id}`
       );
       return await response.json();
     },
@@ -14,6 +17,7 @@ export const PostComponent = ({ postId }: { postId: number }) => {
   return (
     <div>
       <h1>Post</h1>
+      <button onClick={() => setId(id + 1)}>Next Post</button>
       {post.isLoading && <div>Loading</div>}
       {post.isError && <div>Error</div>}
       {post.isSuccess && (

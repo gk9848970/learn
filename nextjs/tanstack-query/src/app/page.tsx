90dfc836ee6cd85f6ec1204869866a7b65b7fe42
gk9848970 <gk9848970@gmail.com>
2025-03-03 15:19:58 +0530

    Using hydration boundary SSR

--- nextjs/tanstack-query/src/app/page.tsx
+++ nextjs/tanstack-query/src/app/page.tsx
@@ -1,20 +1,32 @@
 import { fetchPaginatedPosts } from "@/api";
 import PaginationControls from "@/components/pagination-control";
 import PostsWithQueryPagination from "@/components/posts-query-pagination";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
 
 export default async function Home({
   searchParams,
 }: {
   searchParams: Promise<{ page: number }>;
 }) {
+  const queryClient = new QueryClient();
   const params = await searchParams;
   const currentPage = params.page || 1;
-  const posts = await fetchPaginatedPosts(currentPage);
+
+  await queryClient.prefetchQuery({
+    queryKey: ["posts", currentPage],
+    queryFn: () => fetchPaginatedPosts(currentPage),
+  });
 
   return (
     <div>
       <h1>Page {currentPage}</h1>
-      <PostsWithQueryPagination initialData={posts} page={currentPage} />
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <PostsWithQueryPagination page={currentPage} />
+      </HydrationBoundary>
       <PaginationControls />
     </div>
   );
--- nextjs/tanstack-query/src/components/posts-query-pagination.tsx
+++ nextjs/tanstack-query/src/components/posts-query-pagination.tsx
@@ -1,25 +1,16 @@
 "use client";
 
-import { fetchPaginatedPosts, Post } from "@/api";
+import { fetchPaginatedPosts } from "@/api";
 import { useQuery } from "@tanstack/react-query";
 
 export default function PostsWithQueryPagination({
-  initialData,
   page = 1,
 }: {
-  initialData?: Post[];
   page?: number;
 }) {
   const posts = useQuery({
     queryKey: ["posts", page],
-    queryFn: () => {
-      console.log("Ran");
-      return fetchPaginatedPosts(page);
-    },
-    initialData: () => {
-      console.log("Ran2");
-      return initialData;
-    },
+    queryFn: () => fetchPaginatedPosts(page),
   });
 
   if (posts.isLoading) {

72e4391b95326a636b6adde97dacd0456073d9bf
gk9848970 <gk9848970@gmail.com>
2025-07-15 22:36:07 +0530

    Typescript 50 lessons - 20.5

--- typescript/typescript-in-50-lessons/src/lessons/overloading-functions.ts
+++ typescript/typescript-in-50-lessons/src/lessons/overloading-functions.ts
@@ -1,3 +1,4 @@
+// For normal functions
 function search(term: string, tags?: string[]): void;
 
 function search(term: string, callback: () => void, tags?: string[]): void;
@@ -27,6 +28,43 @@ function search(term: string, p1?: (() => void) | string[], p2?: string[]) {
 
   console.log(queryString);
 }
+
+// For arrow functions
+
+type SearchFn = {
+  (term: string, tags?: string[]): void;
+  (term: string, callback: () => void, tags?: string[]): void;
+};
+
+const searchArrowFn = (
+  term: string,
+  p1?: (() => void) | string[],
+  p2?: string[]
+) => {
+  const callback = typeof p1 === "function" ? p1 : undefined;
+
+  const tags =
+    typeof p1 !== undefined && Array.isArray(p1)
+      ? p1
+      : typeof p2 !== undefined && Array.isArray(p2)
+      ? p2
+      : [];
+
+  let queryString = `?query=${term}`;
+
+  if (tags.length) {
+    console.log("Tags are given");
+    queryString += `&tags=${tags.join()}`;
+  }
+
+  if (callback) {
+    callback();
+  } else {
+    console.log("callback is not a function");
+  }
+
+  console.log(queryString);
+};
 
 console.log(1);
 search("Batman");

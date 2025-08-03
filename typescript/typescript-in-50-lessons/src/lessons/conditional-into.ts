506c917d864249cafa9e1decee029f5894038653
gk9848970 <gk9848970@gmail.com>
2025-08-03 11:31:35 +0530

    Typescript 50 lessons - 36

---
+++ typescript/typescript-in-50-lessons/src/lessons/conditional-into.ts
@@ -0,0 +1,54 @@
+type Customer = {
+  customerId: number;
+  firstName: string;
+  lastName: string;
+};
+
+const customer: Customer = {
+  customerId: 1,
+  firstName: "Stefan",
+  lastName: "Baumgartner",
+};
+
+type Product = {
+  productId: number;
+  title: string;
+  price: number;
+};
+
+const product: Product = {
+  productId: 22,
+  title: "Form Design Patterns",
+  price: 29,
+};
+
+type Order = {
+  orderId: number;
+  customer: Customer;
+  products: Product[];
+  date: Date;
+};
+
+// function fetchOrder(customer: Customer): Order[];
+// function fetchOrder(product: Product): Order[];
+// function fetchOrder(orderId: number): Order;
+
+type FetchParam = number | Customer | Product;
+type FetchReturnParam<T extends FetchParam> = T extends number
+  ? Order
+  : Order[];
+
+// Nested tertiary statements
+type FetchReturnParamComplex<T extends FetchParam> = T extends Customer
+  ? Order[]
+  : T extends Product
+  ? Order[]
+  : Order;
+
+declare function fetchOrder<Param extends FetchParam>(
+  param: Param
+): FetchReturnParam<Param>;
+
+const first = fetchOrder(customer);
+const second = fetchOrder(product);
+const third = fetchOrder(1);

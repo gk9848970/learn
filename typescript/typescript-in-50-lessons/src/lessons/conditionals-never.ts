2ea011a38707104353cdec252b009073a3642544
gk9848970 <gk9848970@gmail.com>
2025-08-09 17:17:12 +0530

    Typescript 50 lessons - 40

--- typescript/typescript-in-50-lessons/src/lessons/conditionals-never.ts
+++ typescript/typescript-in-50-lessons/src/lessons/conditionals-never.ts
@@ -1,1 +1,65 @@
+type Medium = {
+  id: number;
+  title: string;
+  artist: string;
+};
 
+type TrackInfo = {
+  duration: number;
+  tracks: number;
+};
+
+type CD = Medium &
+  TrackInfo & {
+    kind: "cd";
+  };
+
+type LP = Medium & {
+  sides: {
+    a: TrackInfo;
+    b: TrackInfo;
+  };
+  kind: "lp";
+};
+
+type AllMedia = CD | LP;
+type MediaKinds = AllMedia["kind"];
+type AllMediaInfoToPass<TMedia extends AllMedia> = Omit<TMedia, "id" | "kind">;
+type SelectBranchSimple<TKind extends MediaKinds> = TKind extends "cd"
+  ? CD
+  : LP;
+
+function createMedium<TKind extends MediaKinds>(
+  kind: TKind,
+  info: AllMediaInfoToPass<SelectBranchSimple<TKind>>
+): AllMedia;
+/*
+// Check type of autocomplete
+createMedium("cd", {
+    ""
+});
+
+createMedium("lp", {
+    ""
+});
+*/
+
+type SelectBranch<Brnch, Kin> = Brnch extends { kind: Kin } ? Brnch : never;
+
+// CD | LP extends { kind: "cd" } -> CD | never
+type ABC = SelectBranch<AllMedia, "cd">;
+
+function createMediumTwo<TKind extends MediaKinds>(
+  kind: TKind,
+  info: AllMediaInfoToPass<SelectBranch<AllMedia, TKind>>
+): SelectBranch<AllMedia, TKind>;
+
+createMediumTwo("cd");
+
+// SelectBranch can also be written with help of Extact<A, B> defined as
+// Extract<A, B> = A extends B ? A : never (Can B be extended to A)
+
+function createMediumThree<TKind extends MediaKinds>(
+  kind: TKind,
+  info: AllMediaInfoToPass<Extract<AllMedia, { kind: TKind }>>
+): Extract<AllMedia, { kind: TKind }>;

985e1e136635f639b925fbfcf427088de299d9d4
gk9848970 <gk9848970@gmail.com>
2025-12-21 11:54:36 +0530

    LargeAppDev - With blocking updates

--- large-apps-dev/src/modules/future/hooks/current.tsx
+++ large-apps-dev/src/modules/future/hooks/current.tsx
@@ -1,12 +1,33 @@
 import { useState, type SyntheticEvent } from "react";
+import { SampleButton } from "./sample-button";
 
 const submitForm = async () => {
+  console.log("Starting first loop");
+  for (let i = 0; i < 1000000000; i++) {
+    // Do nothing
+  }
+
+  console.log("Starting second loop");
+  for (let i = 0; i < 1000000000; i++) {
+    // Do nothing
+  }
+
+  console.log("Starting third loop");
+  for (let i = 0; i < 1000000000; i++) {
+    // Do nothing
+  }
+
+  console.log("Starting fourth loop");
+  for (let i = 0; i < 1000000000; i++) {
+    // Do nothing
+  }
+
   const promise = new Promise((resolve) => {
     setTimeout(() => {
       resolve({
         time: Date.now(),
       });
-    }, 4000);
+    }, 5000);
   });
 
   return promise;
@@ -32,11 +53,14 @@ export function CurrentForm() {
 
   // display form template
   return (
-    <form onSubmit={formAction}>
-      <input disabled={isPending} id="name" />
-      <div>{JSON.stringify(formState)}</div>
-      {isPending && <div>Form is getting submitted please wait</div>}
-      <button disabled={isPending}>Submit</button>
-    </form>
+    <>
+      <form onSubmit={formAction}>
+        <input disabled={isPending} id="name" />
+        <div>{JSON.stringify(formState)}</div>
+        {isPending && <div>Form is getting submitted please wait</div>}
+        <button disabled={isPending}>Submit</button>
+      </form>
+      <SampleButton />
+    </>
   );
 }

c1d944eb094286bb195cb3b790a4505bb40ab8b2
Gaurav Kumar <gk9848970@gmail.com>
2026-03-01 11:43:46 +0530

    Returning and insertion

--- typescript/drizzle-orm/src/main.ts
+++ typescript/drizzle-orm/src/main.ts
@@ -2,16 +2,31 @@ import { db } from './db';
 import { userTable } from './db/schema';
 
 async function main() {
-  const inserted = await db.insert(userTable).values({
+  await db.delete(userTable);
+
+  const inserted = await db.insert(userTable).values([{
     name: 'Alice',
-    age: 30,
+    age: 31,
+    email: 'alice@example1.com',
+  }, {
+    name: 'Gaurav',
+    age: 32,
+    email: 'alice@example2.com',
+  }, {
+    name: 'Khyati',
+    age: 33,
     email: 'alice@example3.com',
+  }])
+  .returning()
+  .onConflictDoUpdate({
+    target: [userTable.email],
+    set: {
+      name: 'Gaurav Kumar',
+    },
   });
 
+  
   console.log('Inserted:', inserted);
-
-  const users = await db.select().from(userTable);
-  console.log('All users:', users);
 }
 
 main().catch(console.error).finally(() => process.exit());
